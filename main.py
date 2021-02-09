aa48b07b5a10b67c9b279c0e77a0cacd6f3bd767
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-09 06:42:37 +0000

    Add function to get coordinates

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ screen.setup(width=600, height=800)
 screen.title("Name the Countries in Africa")
 
 # Add image as the background
-image = "africa.gif"
+image = "plain_map_of_africa.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+
+# Get the x and y coordinates
+def mouse_click_coor(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(mouse_click_coor)
+turtle.mainloop()
+
 # Keep the screen open until exited
-screen.exitonclick()
+# screen.exitonclick()
