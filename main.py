6298835c1feb5bdb2d44babee59e9602ebdabb94
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-09 06:58:25 +0000

    Add user prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 # Screen default swt up
 screen = turtle.Screen()
@@ -10,14 +11,17 @@ image = "plain_map_of_africa.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+guess = screen.textinput(title="Label the Map", prompt="Guess a country in africa").title()
+print(guess)
 
-# Get the x and y coordinates
+
+"""# Get the x and y coordinates
 def mouse_click_coor(x, y):
     print(x, y)
 
 
 turtle.onscreenclick(mouse_click_coor)
-turtle.mainloop()
+turtle.mainloop()"""
 
 # Keep the screen open until exited
-# screen.exitonclick()
+screen.exitonclick()
